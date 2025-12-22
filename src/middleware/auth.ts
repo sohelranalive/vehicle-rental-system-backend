b772623403ec097407ef419bd5f40c571e4760af
sohelranalive <sohel.cse37@gmail.com>
2025-12-23 00:50:25 +0100

    made the correction

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -6,31 +6,63 @@ import { pool } from "../config/db";
 const auth = (...roles: string[]) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const token = req.headers.authorization;
-      if (!token) {
-        return res.status(403).json({
-          success: false,
-          message: "You are not authorized",
-        });
-      }
-      const verifiedToken = jwt.verify(
-        token,
-        config.jwt_secret as string
-      ) as JwtPayload;
-      req.currentUser = verifiedToken;
+      const authToken = req.headers.authorization;
+      if (authToken && authToken.startsWith("Bearer ")) {
+        const token = authToken.split(" ")[1] as string;
 
-      const result = await pool.query(`SELECT * FROM Users WHERE email=$1`, [
-        req.currentUser.email,
-      ]);
+        const verifiedToken = jwt.verify(
+          token,
+          config.jwt_secret as string
+        ) as JwtPayload;
+        req.currentUser = verifiedToken;
 
-      if (roles.includes(verifiedToken.role) && result.rows.length > 0) {
-        next();
+        const result = await pool.query(`SELECT * FROM Users WHERE email=$1`, [
+          req.currentUser.email,
+        ]);
+
+        if (roles.includes(verifiedToken.role) && result.rows.length > 0) {
+          next();
+        } else {
+          return res.status(403).json({
+            success: false,
+            message: "Unverified, you are not authorized",
+          });
+        }
       } else {
         return res.status(403).json({
           success: false,
-          message: "Unverified, you are not authorized",
+          message: "No bearer token found or invalid token",
         });
       }
+
+      // try {
+      //   console.log(req.headers.authorization);
+      //   const token = req.headers.authorization;
+      //   if (!token) {
+      //     return res.status(403).json({
+      //       success: false,
+      //       message: "You are not authorized",
+      //     });
+      //   }
+
+      //   const verifiedToken = jwt.verify(
+      //     token,
+      //     config.jwt_secret as string
+      //   ) as JwtPayload;
+      //   req.currentUser = verifiedToken;
+
+      //   const result = await pool.query(`SELECT * FROM Users WHERE email=$1`, [
+      //     req.currentUser.email,
+      //   ]);
+
+      //   if (roles.includes(verifiedToken.role) && result.rows.length > 0) {
+      //     next();
+      //   } else {
+      //     return res.status(403).json({
+      //       success: false,
+      //       message: "Unverified, you are not authorized",
+      //     });
+      //   }
     } catch (error: any) {
       res.status(500).json({
         success: false,
